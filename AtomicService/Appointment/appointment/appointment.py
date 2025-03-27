9d18c837224ebf5fe5604fb8e9c55fa9a8300411
kylieyangg <kylie.yang.2023@smu.edu.sg>
2025-03-28 02:06:00 +0800

    updated the ports for appointment and patient for the .py file and compose.yaml

--- AtomicService/Appointment/appointment/appointment.py
+++ AtomicService/Appointment/appointment/appointment.py
@@ -228,7 +228,7 @@ def update_appointment_status(appointment_id):
 if __name__ == '__main__':
     with app.app_context(): #push an application context
         db.create_all() #create table 
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
 
 
 
